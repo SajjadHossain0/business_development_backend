743f16bf180b6858bf3e404c14000722a77c6c27
mdsaj <mdsajjadhossain614@gmail.com>
2025-01-05 13:53:19 +0600

    login and registration

--- src/main/java/com/business_development_backend/business_development_backend/Admin/Controllers/UserController.java
+++ src/main/java/com/business_development_backend/business_development_backend/Admin/Controllers/UserController.java
@@ -1,24 +1,21 @@
 package com.business_development_backend.business_development_backend.Admin.Controllers;
 
-import com.business_development_backend.business_development_backend.Admin.Entities.User;
+import com.business_development_backend.business_development_backend.Admin.Repositories.UserRepository;
 import com.business_development_backend.business_development_backend.Admin.Services.UserDataService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
 
-    private final UserDataService userDataService;
+    @Autowired
+    private UserDataService userDataService;
 
-    public UserController(UserDataService userDataService) {
-        this.userDataService = userDataService;
-    }
+    @Autowired
+    private UserRepository userRepository;
+
 
 
 }

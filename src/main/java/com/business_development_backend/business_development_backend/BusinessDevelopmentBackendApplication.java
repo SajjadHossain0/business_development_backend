d3584dea766c3f8fa030c110660365691eef58d8
mdsaj <mdsajjadhossain614@gmail.com>
2025-01-05 12:00:48 +0600

    first commit

---
+++ src/main/java/com/business_development_backend/business_development_backend/BusinessDevelopmentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.business_development_backend.business_development_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessDevelopmentBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BusinessDevelopmentBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/business_development_backend/business_development_backend/BusinessDevelopmentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.business_development_backend.business_development_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessDevelopmentBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
